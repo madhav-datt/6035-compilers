6a559a603c552a3c1137907ca819707383c4e1c4
Devin Morgan <devinm@mit.edu>
2016-10-06 01:14:38 -0400

    Created IrCtrlFlowFor class

---
+++ src/edu/mit/compilers/ir/IrCtrlFlowWhile.java
@@ -0,0 +1,10 @@
+package edu.mit.compilers.ir;
+
+/**
+ * Created by devinmorgan on 10/5/16.
+ */
+public class IrCtrlFlowWhile extends IrCtrlFlow{
+    public IrCtrlFlowWhile(IrExpr condExpr, IrCodeBlock stmtBody, int lineNumber, int colNumber) {
+        super(condExpr, stmtBody, lineNumber, colNumber);
+    }
+}
