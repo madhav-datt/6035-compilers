940ba3844b228e362e6f932b03e09314d7c23cbd
Devin Morgan <devinm@mit.edu>
2016-10-23 15:30:36 -0400

    Added semanticCheck for IrStmtContinue and IrStmtBreak

--- src/edu/mit/compilers/ir/IrStmtBreak.java
+++ src/edu/mit/compilers/ir/IrStmtBreak.java
@@ -1,10 +1,26 @@
 package edu.mit.compilers.ir;
 
+import edu.mit.compilers.ScopeStack;
+
 /**
  * Created by devinmorgan on 10/5/16.
  */
 public class IrStmtBreak extends IrStatement {
     public IrStmtBreak(int lineNumber, int colNumber) {
         super(lineNumber, colNumber);
+    }
+
+    @Override
+    public String semanticCheck(ScopeStack scopeStack) {
+        String errorMessage = "";
+
+        // 1) check to see if this statement resides in a loop scope or not
+        boolean isInALoop = scopeStack.isScopeForALoop();
+        if (!isInALoop) {
+            errorMessage += "Break statement cannot be used outside of a for or while loop" +
+                    " line: "+this.getLineNumber() + " col: " +this.getColNumber() + "\n";
+        }
+
+        return errorMessage;
     }
 }
--- src/edu/mit/compilers/ir/IrStmtContinue.java
+++ src/edu/mit/compilers/ir/IrStmtContinue.java
@@ -1,10 +1,26 @@
 package edu.mit.compilers.ir;
 
+import edu.mit.compilers.ScopeStack;
+
 /**
  * Created by devinmorgan on 10/5/16.
  */
 public class IrStmtContinue extends IrStatement {
     public IrStmtContinue(int lineNumber, int colNumber) {
         super(lineNumber, colNumber);
+    }
+
+    @Override
+    public String semanticCheck(ScopeStack scopeStack) {
+        String errorMessage = "";
+
+        // 1) check to see if this statement resides in a loop scope or not
+        boolean isInALoop = scopeStack.isScopeForALoop();
+        if (!isInALoop) {
+            errorMessage += "Continue statement cannot be used outside of a for or while loop" +
+                    " line: "+this.getLineNumber() + " col: " +this.getColNumber() + "\n";
+        }
+
+        return errorMessage;
     }
 }
