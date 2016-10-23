5ce91c9377e036fb5e079905c0cf444cbfc2c457
Devin Morgan <devinm@mit.edu>
2016-10-23 13:09:34 -0400

    Filled in semanticCheck for IrArg classes

--- src/edu/mit/compilers/ir/IrArgExpr.java
+++ src/edu/mit/compilers/ir/IrArgExpr.java
@@ -1,10 +1,19 @@
 package edu.mit.compilers.ir;
 
+import edu.mit.compilers.ScopeStack;
+
 /**
  * Created by devinmorgan on 10/16/16.
  */
 public class IrArgExpr extends IrArg {
     public IrArgExpr(IrExpr expr, int lineNum, int colNum) {
         super(expr, lineNum, colNum);
+    }
+
+    @Override
+    public String semanticCheck(ScopeStack scopeStack) {
+        String errorMessage = ((IrExpr) this.getArgValue()).semanticCheck(scopeStack);
+
+        return errorMessage;
     }
 }
--- src/edu/mit/compilers/ir/IrArgString.java
+++ src/edu/mit/compilers/ir/IrArgString.java
@@ -1,10 +1,17 @@
 package edu.mit.compilers.ir;
 
+import edu.mit.compilers.ScopeStack;
+
 /**
  * Created by devinmorgan on 10/16/16.
  */
 public class IrArgString extends IrArg {
     public IrArgString(String string, int lineNum, int colNum) {
         super(string, lineNum, colNum);
+    }
+
+    @Override
+    public String semanticCheck(ScopeStack scopeStack) {
+        return "";
     }
 }
