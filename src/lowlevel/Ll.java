8bfacf6bf9ceaec07e96d201ab27effa537979ad
Devin Morgan <devinm@mit.edu>
2016-11-18 19:30:45 -0500

    Created the helper classes for LowerLevelIr: LlLiteral and LlLocation

---
+++ src/lowlevel/Ll.java
@@ -0,0 +1,7 @@
+package lowlevel;
+
+/**
+ * Created by devinmorgan on 11/18/16.
+ */
+public abstract class Ll {
+}
---
+++ src/lowlevel/LlLocationArray.java
@@ -0,0 +1,7 @@
+package lowlevel;
+
+/**
+ * Created by devinmorgan on 11/18/16.
+ */
+public class LlLocationArray extends LlLocation{
+}
