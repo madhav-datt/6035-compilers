636f70813da258bac412ea61b9de76fe1d673be5
Devin Morgan <devinm@mit.edu>
2016-11-18 19:49:40 -0500

    Populated the LlLiteral classes

--- src/lowlevel/LlLiteralBool.java
+++ src/lowlevel/LlLiteralBool.java
@@ -4,4 +4,9 @@ package lowlevel;
  * Created by devinmorgan on 11/18/16.
  */
 public class LlLiteralBool extends LlLiteral{
+    private final boolean boolValue;
+
+    public LlLiteralBool(boolean boolValue) {
+        this.boolValue = boolValue;
+    }
 }
--- src/lowlevel/LlLiteralInt.java
+++ src/lowlevel/LlLiteralInt.java
@@ -4,4 +4,9 @@ package lowlevel;
  * Created by devinmorgan on 11/18/16.
  */
 public class LlLiteralInt extends LlLiteral{
+    private final int intValue;
+
+    public LlLiteralInt(int intValue) {
+        this.intValue = intValue;
+    }
 }
