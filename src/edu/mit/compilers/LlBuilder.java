bde8ea9f18b84724af949a8f0e7f8c1e74af30d2
Devin Morgan <devinm@mit.edu>
2016-11-20 22:06:49 -0500

    Swapped out the depreciated OrderedHashMap with the LinkedHashMap

--- src/edu/mit/compilers/LlBuilder.java
+++ src/edu/mit/compilers/LlBuilder.java
@@ -4,21 +4,41 @@ import edu.mit.compilers.ll.LlLocation;
 import edu.mit.compilers.ll.LlLocationVar;
 import edu.mit.compilers.ll.LlStatement;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Created by abel on 11/18/16.
  */
 public class LlBuilder {
-    private List<LlStatement> statementList;
+    private LinkedHashMap<String, LlStatement> statementTable;
     private int labelCounter = 0;
     private int tempCounter = 0;
 
+    private Stack<String> currentBlockLabel = new Stack<>();
+
+    public LlBuilder(){
+        this.statementTable = new LinkedHashMap<>();
+    }
+
+
     // Add to the end of the list
+
     public void appendStatement(LlStatement statement){
-        statementList.add(statement);
+        String label = this.generateLabel();
+        statementTable.put(label, statement);
+    }
+
+    public void appendStatement(String label, LlStatement statement){
+        if(statementTable.containsKey(label)){
+            System.err.println("Duplicate label key . Please use the label generator! ");
+            System.err.println("Key :" + label);
+            System.err.println("Statement : " + statement.toString());
+            System.err.println("StackSize "+ labelCounter);
+
+        }
+        else{
+            statementTable.put(label, statement);
+        }
     }
 
     //Generates a label
@@ -31,9 +51,34 @@ public class LlBuilder {
         return new LlLocationVar("t"+Integer.toString(tempCounter++));
     }
 
-    // Get the LlStatments List
+    public LinkedHashMap<String, LlStatement> getStatementTable() {
+        return statementTable;
+    }
 
-    public ArrayList<LlStatement> getStatementList() {
-        return new ArrayList<LlStatement>(this.statementList);
+    // Call this when you get out of a block.
+    public void getOutOfBlock(){
+        currentBlockLabel.pop();
+    }
+
+    public String getCurrentBlock(){
+        if(currentBlockLabel.size() > 0){
+            return currentBlockLabel.peek();
+        }
+        return null;
+    }
+
+    public void getInBlock(String loopLabel){
+        currentBlockLabel.push(loopLabel);
+    }
+
+    @Override
+    public String toString() {
+        String st = "";
+        for(String key : statementTable.keySet()){
+            st += String.format("%1$15s :  ", key);
+            st += statementTable.get(key) + "\n";
+
+        }
+        return st;
     }
 }
