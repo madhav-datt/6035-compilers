c3fbd541520b941f63eb583da595ef8a9667f93c
Devin Morgan <devinm@mit.edu>
2016-12-03 23:39:53 -0500

    Completed the AvailableExpressions algorithm

--- src/edu/mit/compilers/cfg/AvailableExpressions.java
+++ src/edu/mit/compilers/cfg/AvailableExpressions.java
@@ -2,29 +2,96 @@ package edu.mit.compilers.cfg;
 
 import edu.mit.compilers.ll.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
+import java.util.*;
 
 /**
  * Created by devinmorgan on 12/3/16.
  */
 public class AvailableExpressions {
+    private HashSet<Computation> universalSet = new HashSet<>();
+    private final HashMap<BasicBlock, HashSet<Computation>> availExprIN = new HashMap<>();
+    private final HashMap<BasicBlock, HashSet<Computation>> availExprOUT = new HashMap<>();
 
-    public ArrayList<HashSet<Computation>> getAvailableExpressionsForCFG(CFG cfg) {
-        /*
-            1) iterate over all the basic blocks in the CFG to create EVAL(i)
-         */
+    private AvailableExpressions(CFG cfg) {
+        ArrayList<BasicBlock> bbList = cfg.getBasicBlocks();
+
+        // 1) initial allExpressions with the union of all EVAL(bb)'s
+        for (BasicBlock bb : bbList) {
+            this.universalSet.addAll(EVAL(bb));
+        }
+
+        // 2) perform the worklist algorithm
+
+        // OUT[n] = E for all nodes
+        for (BasicBlock bb: bbList) {
+            this.availExprOUT.put(
+                    bb, new HashSet<>(this.universalSet) // prevent mutability issues
+            );
+        }
+
+        ArrayList<BasicBlock> activeNodes = new ArrayList<>(bbList);
+        BasicBlock entry = activeNodes.get(0);
+        
+        // IN[Entry] = emptyset; 
+        this.availExprIN.put(entry, new HashSet<>());
+        
+        // OUT[Entry] = EVAL[Entry]; 
+        this.availExprOUT.put(entry, EVAL(entry));
+        
+        // Changed = N - {Entry};
+        activeNodes.remove(0); // remove the entry since it was accounted for
+
+        while (activeNodes.size() > 0) {
+
+            // get a node and remove it from active nodes
+            BasicBlock node = activeNodes.get(0);
+            activeNodes.remove(0);
+            HashSet<Computation> oldOUT = this.availExprOUT.get(node);
+
+            // IN[n] = E
+            // IN[n] = IN[n] intersect OUT[p] for all p in predecessors
+            HashSet<Computation> IN = new HashSet<>(this.universalSet);
+            for (BasicBlock pred : node.getPredecessors()) {
+                IN.removeAll(this.availExprOUT.get(pred));
+            }
+            this.availExprIN.put(node, IN);
+
+            // KILL[n]
+            HashSet<Computation> KILL = KILL(node, new HashSet<>(this.universalSet));
+
+            // (IN[n]-KILL[n])
+            HashSet<Computation> INminusKILL = new HashSet<>(this.availExprIN.get(node)); // make separate copy of IN
+            INminusKILL.removeAll(KILL);
+            
+            // OUT[n] = EVAL[n] U (IN[n]-KILL[n])
+            HashSet<Computation> EVALplusINminusKILL = EVAL(node);
+            EVALplusINminusKILL.addAll(INminusKILL);
+            this.availExprOUT.put(node, EVALplusINminusKILL);
+
+            // if OUT[n] changed, add its successors to activeNodes
+            if (!this.availExprOUT.equals(oldOUT)) {
+                if (node.getDefaultBranch() != null) {
+                    activeNodes.add(node.getDefaultBranch());
+                }
+                if (node.getAlternativeBranch() != null) {
+                    activeNodes.add(node.getAlternativeBranch());
+                }
+            }
+        }
+    }
+
+    public static HashMap<BasicBlock, HashSet<Computation>> getAvailableExpressionsForCFG(CFG cfg) {
+        AvailableExpressions ae = new AvailableExpressions(cfg);
+        return ae.availExprIN;
     }
 
     // returns the set of expressions that are generated
     // in the BasicBlock AND are still available at the
     // end of the BasicBlock
-    public HashSet<Computation> EVAL(BasicBlock bb) {
+    private HashSet<Computation> EVAL(BasicBlock bb) {
         HashSet<Computation> generatedExpressions = new HashSet<>();
-
         LinkedHashMap<String, LlStatement> labelsToStmtsMap = bb.getLabelsToStmtsMap();
+
         for (String label : labelsToStmtsMap.keySet()) {
             LlStatement stmt = labelsToStmtsMap.get(label);
 
@@ -65,5 +132,29 @@ public class AvailableExpressions {
 
         }
         return generatedExpressions;
+    }
+
+    // returns the sub-set of passed set of expressions that are killed
+    // by this BasicBlock
+    private HashSet<Computation> KILL(BasicBlock bb, HashSet<Computation> superSet) {
+        HashSet<Computation> killedExpressions = new HashSet<>();
+        LinkedHashMap<String, LlStatement> labelsToStmtsMap = bb.getLabelsToStmtsMap();
+
+        for (String label : labelsToStmtsMap.keySet()) {
+            LlStatement stmt = labelsToStmtsMap.get(label);
+
+            // add all the computations that contain operands
+            // that get re-assigned by the current stmt to
+            // killedExpressions
+            if (stmt instanceof LlAssignStmt) {
+                LlAssignStmt assignStmt = (LlAssignStmt) stmt;
+                for (Computation comp : superSet) {
+                    if (comp.contains(assignStmt.getStoreLocation())) {
+                        killedExpressions.add(comp);
+                    }
+                }
+            }
+        }
+        return killedExpressions;
     }
 }
