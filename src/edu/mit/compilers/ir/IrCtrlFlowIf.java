07ee867c9cd6fdcc71a8d3c2f71b374b63ec34a2
Devin Morgan <devinm@mit.edu>
2016-12-11 21:01:56 -0500

    Enabled short-circuiting when the outer most layer is at least a binary expresssion

--- src/edu/mit/compilers/ir/IrCtrlFlowIf.java
+++ src/edu/mit/compilers/ir/IrCtrlFlowIf.java
@@ -3,6 +3,8 @@ package edu.mit.compilers.ir;
 import edu.mit.compilers.*;
 import edu.mit.compilers.ll.*;
 
+import java.util.ArrayList;
+
 /**
  * Created by devinmorgan on 10/5/16.
  */
@@ -62,29 +64,84 @@ public class IrCtrlFlowIf extends IrCtrlFlow {
 
     @Override
     public LlLocation generateLlIr(LlBuilder builder, LlSymbolTable symbolTable) {
-        // condition goto(label)
-        // if not go to end of the if block
-        String ifBlockLabel = "IF_" + builder.generateLabel();
-        String endIfLabel = "END_" + ifBlockLabel;
 
-        // Generate the conditional statement.
-        LlLocation conditionalTemp = this.condExpr.generateLlIr(builder, symbolTable);
-        LlJumpConditional conditionalJump = new LlJumpConditional(ifBlockLabel, conditionalTemp);
-        builder.appendStatement(conditionalJump);
+        // short-circuit if the expr is a BinaryArith
+        if (this.condExpr instanceof IrOperBinaryCond) {
+            IrOperBinaryCond binaryOp = (IrOperBinaryCond) this.condExpr;
+            IrExpr op1 = binaryOp.getLeftOperand();
+            IrExpr op2 = binaryOp.getRightOperand();
 
-        // if the conditional doesnt work, jump to the end of the if block.
-        LlJumpUnconditional unconditionalJump = new LlJumpUnconditional(endIfLabel);
-        builder.appendStatement(unconditionalJump);
-        // add the label to the if body block
-        LlEmptyStmt emptyStmt = new LlEmptyStmt();
-        builder.appendStatement(ifBlockLabel, emptyStmt);
+            if (binaryOp.getOperation().equals("&&")) {
 
-        //  finally generate the if statement body itself
-        this.stmtBody.generateLlIr(builder, symbolTable);
+                // if (op2) --> this.stmtBody
+                IrCtrlFlowIf op2If = new IrCtrlFlowIf(op2, this.stmtBody);
 
-        // append end if label
-        LlEmptyStmt endIfEmptyStmt = new LlEmptyStmt();
-        builder.appendStatement(endIfLabel, endIfEmptyStmt);
+                // else --> do nothing
+                IrCodeBlock emptyBlock = new IrCodeBlock(new ArrayList<>(), new ArrayList<>(), this.getLineNumber(), this.getColNumber());
+                IrCtrlFlowIfElse op2Else = new IrCtrlFlowIfElse(op2If, emptyBlock);
+
+                // if (op1) --> if (op2)
+                ArrayList<IrStatement> stmtsList = new ArrayList<>();
+                stmtsList.add(op2Else);
+                IrCodeBlock op1IfBlock = new IrCodeBlock(new ArrayList<>(), stmtsList, this.getLineNumber(), this.getColNumber());
+                IrCtrlFlowIf op1If = new IrCtrlFlowIf(op1, op1IfBlock);
+
+                // else --> do nothing
+                IrCtrlFlowIfElse op1IfElse = new IrCtrlFlowIfElse(op1If, emptyBlock);
+
+                // create the short-circuit code instead
+                op1IfElse.generateLlIr(builder, symbolTable);
+            }
+            else if (binaryOp.getOperation().equals("||")) {
+                IrOperUnaryNot notOp1 = new IrOperUnaryNot(op1);
+                IrOperUnaryNot notOp2 = new IrOperUnaryNot(op1);
+                IrCodeBlock emptyBlock = new IrCodeBlock(new ArrayList<>(), new ArrayList<>(), this.getLineNumber(), this.getColNumber());
+
+                // if (notOp2) --> do nothing
+                IrCtrlFlowIf notOp2If = new IrCtrlFlowIf(notOp2, emptyBlock);
+
+                // else --> this.stmtBody
+                IrCtrlFlowIfElse notOp2IfElse = new IrCtrlFlowIfElse(notOp2If, this.stmtBody);
+
+                // if (notOp1) --> if (notOp2)
+                ArrayList<IrStatement> stmstList = new ArrayList<>();
+                stmstList.add(notOp2IfElse);
+                IrCodeBlock notOp1IfBlock = new IrCodeBlock(new ArrayList<>(), stmstList, this.getLineNumber(), this.getColNumber());
+                IrCtrlFlowIf notOp1If = new IrCtrlFlowIf(notOp1, notOp1IfBlock);
+
+                // else --> this.stmtBody
+                IrCtrlFlowIfElse notOp1IfElse = new IrCtrlFlowIfElse(notOp1If, this.stmtBody);
+
+                // generate the short-circuit code
+                notOp1IfElse.generateLlIr(builder, symbolTable);
+            }
+        }
+        else {
+            // condition goto(label)
+            // if not go to end of the if block
+            String ifBlockLabel = "IF_" + builder.generateLabel();
+            String endIfLabel = "END_" + ifBlockLabel;
+
+            // Generate the conditional statement.
+            LlLocation conditionalTemp = this.condExpr.generateLlIr(builder, symbolTable);
+            LlJumpConditional conditionalJump = new LlJumpConditional(ifBlockLabel, conditionalTemp);
+            builder.appendStatement(conditionalJump);
+
+            // if the conditional doesnt work, jump to the end of the if block.
+            LlJumpUnconditional unconditionalJump = new LlJumpUnconditional(endIfLabel);
+            builder.appendStatement(unconditionalJump);
+
+            // add the label to the if body block
+            LlEmptyStmt emptyStmt = new LlEmptyStmt();
+            builder.appendStatement(ifBlockLabel, emptyStmt);
+
+            // finally generate the if statement body itself
+            this.stmtBody.generateLlIr(builder, symbolTable);
+
+            // append end if label
+            LlEmptyStmt endIfEmptyStmt = new LlEmptyStmt();
+            builder.appendStatement(endIfLabel, endIfEmptyStmt);
+        }
 
         // return the last known ...
         return null;
--- src/edu/mit/compilers/ir/IrCtrlFlowIfElse.java
+++ src/edu/mit/compilers/ir/IrCtrlFlowIfElse.java
@@ -3,6 +3,8 @@ package edu.mit.compilers.ir;
 import edu.mit.compilers.*;
 import edu.mit.compilers.ll.*;
 
+import java.util.ArrayList;
+
 /**
  * Created by devinmorgan on 10/5/16.
  */
@@ -53,37 +55,91 @@ public class IrCtrlFlowIfElse extends IrCtrlFlow{
     // TODO: Check
     @Override
     public LlLocation generateLlIr(LlBuilder builder, LlSymbolTable symbolTable) {
-        // condition goto(label)
-        // if not go to end of the if block
-        String ifBlockLabel = "IF_" + builder.generateLabel();
-        String endIfLabel = "END_" + ifBlockLabel;
 
-        String elseBlockLabel = "ELSE_IF_" + builder.generateLabel();
+        if (this.condExpr instanceof IrOperBinaryCond) {
+            IrOperBinaryCond binaryOp = (IrOperBinaryCond) this.condExpr;
+            IrExpr op1 = binaryOp.getLeftOperand();
+            IrExpr op2 = binaryOp.getRightOperand();
 
-        // Generate the conditional statement.
-        LlLocation conditionalTemp = this.condExpr.generateLlIr(builder, symbolTable);
-        LlJumpConditional conditionalJump = new LlJumpConditional(ifBlockLabel, conditionalTemp);
-        builder.appendStatement(conditionalJump);
+            if (binaryOp.getOperation().equals("&&")) {
+                IrCodeBlock emptyBlock = new IrCodeBlock(new ArrayList<>(), new ArrayList<>(), this.getLineNumber(), this.getColNumber());
 
-        LlEmptyStmt elseLabelEmptyStmt = new LlEmptyStmt();
-        LlJumpUnconditional unconditionalJumpToElse = new LlJumpUnconditional(elseBlockLabel);
-        builder.appendStatement(unconditionalJumpToElse);
-        builder.appendStatement(elseBlockLabel, elseLabelEmptyStmt);
-        this.elseBlock.generateLlIr(builder, symbolTable);
+                // if (op2) --> this.stmtBody
+                IrCtrlFlowIf op2If = new IrCtrlFlowIf(op2, this.stmtBody);
 
-        // after the else block is executed, jump to the end of the if block.
-        LlJumpUnconditional unconditionalJump = new LlJumpUnconditional(endIfLabel);
+                // else --> do nothing
+                IrCtrlFlowIfElse op2IfElse = new IrCtrlFlowIfElse(op2If, emptyBlock);
 
-        // add the label to the if body block
-        LlEmptyStmt emptyStmt = new LlEmptyStmt();
-        builder.appendStatement(ifBlockLabel, emptyStmt);
+                // if (op1) --> if (op2)
+                ArrayList<IrStatement> stmtsList = new ArrayList<>();
+                stmtsList.add(op2IfElse);
+                IrCodeBlock op1IfBlock = new IrCodeBlock(new ArrayList<>(), stmtsList, this.getLineNumber(), this.getColNumber());
+                IrCtrlFlowIf op1If = new IrCtrlFlowIf(op1, op1IfBlock);
 
-        //  finally generate the if statement body itself
-        this.stmtBody.generateLlIr(builder, symbolTable);
+                // else --> do nothing
+                IrCtrlFlowIfElse op1IfElse = new IrCtrlFlowIfElse(op1If, emptyBlock);
 
-        // append end if label
-        LlEmptyStmt endIfEmptyStmt = new LlEmptyStmt();
-        builder.appendStatement(endIfLabel, endIfEmptyStmt);
+                // generate the short-circuit code
+                op1IfElse.generateLlIr(builder, symbolTable);
+
+            }
+            else if (binaryOp.getOperation().equals("||")) {
+                IrOperUnaryNot notOp1 = new IrOperUnaryNot(op1);
+                IrOperUnaryNot notOp2 = new IrOperUnaryNot(op1);
+
+                // if (notOp2) --> this.elseBlock
+                IrCtrlFlowIf notOp2If = new IrCtrlFlowIf(notOp2, this.elseBlock);
+
+                // else --> this.stmtBody
+                IrCtrlFlowIfElse notOp2IfElse = new IrCtrlFlowIfElse(notOp2If, this.stmtBody);
+
+                // if (notOp1) --> if (notOp2)
+                ArrayList<IrStatement> stmstList = new ArrayList<>();
+                stmstList.add(notOp2IfElse);
+                IrCodeBlock notOp1IfBlock = new IrCodeBlock(new ArrayList<>(), stmstList, this.getLineNumber(), this.getColNumber());
+                IrCtrlFlowIf notOp1If = new IrCtrlFlowIf(notOp1, notOp1IfBlock);
+
+                // else --> this.stmtBody
+                IrCtrlFlowIfElse notOp1IfElse = new IrCtrlFlowIfElse(notOp1If, this.stmtBody);
+
+                // generate the short-circuit code
+                notOp1IfElse.generateLlIr(builder, symbolTable);
+            }
+        }
+        else {
+
+            // condition goto(label)
+            // if not go to end of the if block
+            String ifBlockLabel = "IF_" + builder.generateLabel();
+            String endIfLabel = "END_" + ifBlockLabel;
+
+            String elseBlockLabel = "ELSE_IF_" + builder.generateLabel();
+
+            // Generate the conditional statement.
+            LlLocation conditionalTemp = this.condExpr.generateLlIr(builder, symbolTable);
+            LlJumpConditional conditionalJump = new LlJumpConditional(ifBlockLabel, conditionalTemp);
+            builder.appendStatement(conditionalJump);
+
+            LlEmptyStmt elseLabelEmptyStmt = new LlEmptyStmt();
+            LlJumpUnconditional unconditionalJumpToElse = new LlJumpUnconditional(elseBlockLabel);
+            builder.appendStatement(unconditionalJumpToElse);
+            builder.appendStatement(elseBlockLabel, elseLabelEmptyStmt);
+            this.elseBlock.generateLlIr(builder, symbolTable);
+
+            // after the else block is executed, jump to the end of the if block.
+            LlJumpUnconditional unconditionalJump = new LlJumpUnconditional(endIfLabel);
+
+            // add the label to the if body block
+            LlEmptyStmt emptyStmt = new LlEmptyStmt();
+            builder.appendStatement(ifBlockLabel, emptyStmt);
+
+            //  finally generate the if statement body itself
+            this.stmtBody.generateLlIr(builder, symbolTable);
+
+            // append end if label
+            LlEmptyStmt endIfEmptyStmt = new LlEmptyStmt();
+            builder.appendStatement(endIfLabel, endIfEmptyStmt);
+        }
 
         // return the last known ...
         return null;
